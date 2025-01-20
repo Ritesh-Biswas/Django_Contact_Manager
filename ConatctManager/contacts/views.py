a1e1eeef6d86fdaa303f92d68a72b3f7339e18af
Ritesh Biswas <lusebiswas@gmail.com>
2025-01-20 12:23:46 +0530

    Advance Query SQL Lite is applied

--- ConatctManager/contacts/views.py
+++ ConatctManager/contacts/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect # type: ignore
 from .models import Contacts
-from django.http import HttpResponse
+from django.http import HttpResponse # type: ignore
+from django.db import models # type: ignore
 
 # Create your views here.
 def contact_list(request):
-    contacts = Contacts.objects.all() #fetching all the contacts
-    return render(request,'contacts/contact_list.html',{'contacts':contacts})
+    #contacts = Contacts.objects.all() #fetching all the contacts
+    query = request.GET.get('query', '')
+    contacts = Contacts.objects.filter(
+        models.Q(name__icontains=query) | models.Q(email__icontains=query)
+    )
+    return render(request,'contacts/contact_list.html',{'contacts':contacts,'query': query})
 
 def contact_detail(request,pk):
     contact = get_object_or_404(Contacts,pk=pk)
@@ -18,5 +23,5 @@ def add_contact(request):
         phone = request.POST.get('phone')
         address = request.POST.get('address')
         Contacts.objects.create(name=name, email=email, phone=phone, address=address)
-        return redirect('contact_lis')
+        return redirect('contact_list')
     return render(request, 'contacts/add_contact.html')
