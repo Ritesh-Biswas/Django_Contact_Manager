e12adfc4eac3bee6a0c88d2cd9a64c9daf8a971b
Ritesh Biswas <lusebiswas@gmail.com>
2025-01-20 11:30:40 +0530

    Model and Admin is done for conatcts

--- ConatctManager/contacts/admin.py
+++ ConatctManager/contacts/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+from .models import Contacts
 
 # Register your models here.
+admin.site.register(Contacts)
+
--- ConatctManager/contacts/models.py
+++ ConatctManager/contacts/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+class Contacts(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
