3a029889d2047bc1451e9dd5a4d235ef6b4ad368
Ritesh Biswas <lusebiswas@gmail.com>
2025-01-31 11:46:18 +0530

    UI Updated

--- ConatctManager/contacts/urls.py
+++ ConatctManager/contacts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path # type: ignore
 from . import views
+from .views import contact_list, add_contact, contact_detail, edit_contact
 
 urlpatterns = [
     path('',views.contact_list, name='contact_list'),#Static Route
     path('add/',views.add_contact, name='add_contact'),
-    path('<int:pk>/', views.contact_detail, name='contact_detail')
+    path('<int:pk>/', views.contact_detail, name='contact_detail'),
+    path('<int:contact_id>/edit/', edit_contact, name='edit_contact'),
+    path('<int:contact_id>/delete/', views.delete_contact, name='delete_contact'),
 ]
--- ConatctManager/contacts/views.py
+++ ConatctManager/contacts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect # type: ignore
 from .models import Contacts
 from django.http import HttpResponse # type: ignore
 from django.db import models # type: ignore
+from django import forms # type: ignore
+from django.contrib import messages  # type: ignore
 
 # Create your views here.
 def contact_list(request):
@@ -25,3 +27,32 @@ def add_contact(request):
         Contacts.objects.create(name=name, email=email, phone=phone, address=address)
         return redirect('contact_list')
     return render(request, 'contacts/add_contact.html')
+
+class ContactForm(forms.ModelForm):  
+    class Meta:
+        model = Contacts
+        fields = ['name', 'email', 'phone']  # Including the fields we want to edit
+
+def edit_contact(request, contact_id):
+    contact = get_object_or_404(Contacts, id=contact_id)  # Getting the contact object
+
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)  # Populating the form with existing data
+        if form.is_valid():
+            form.save()  
+            return redirect('contact_list')  # Redirect to contact list after saving
+    else:
+        form = ContactForm(instance=contact)  # Show form with current data
+
+    return render(request, 'contacts/edit_contact.html', {'form': form, 'contact': contact})
+
+
+def delete_contact(request, contact_id):
+    contact = get_object_or_404(Contacts, id=contact_id)
+    
+    if request.method == "POST":
+        contact.delete()
+        messages.success(request, "Contact deleted successfully!")  # Confirmation message
+        return redirect('contact_list')
+
+    return render(request, 'contacts/delete_contact.html', {'contact': contact})
